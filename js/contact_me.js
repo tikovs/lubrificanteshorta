0fd70cf314bd0787d69e7b93fcecd0f20eb14f11
Thiago <thiago.si@live.com>
2016-08-07 11:27:36 -0300

    formspree test

--- js/contact_me.js
+++ js/contact_me.js
@@ -27,7 +27,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "./mail/enviar_email.php",
+                url: "./formspree.io/tikovs@gmail.com",
                 type: "POST",
                 data: {
                     name: name,
